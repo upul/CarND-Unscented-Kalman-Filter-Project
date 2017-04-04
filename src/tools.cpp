6880badc99167a17ce6b457c3f93d34aef32b3e5
Upul Bandara <upulbandara@gmail.com>
2017-04-04 12:03:31 +0530

    feature: UKF RMSE helper function of the UKF is completed

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,7 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using namespace std;
 
 Tools::Tools() {}
 
@@ -11,8 +12,24 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+    // checking dimensions of estimations and ground_truth vectors
+    if(estimations.size() == 0 || (estimations.size() != ground_truth.size())){
+        cout << "Invalid estimation or ground truth data " << endl;
+        return rmse;
+    }
+
+    // calculate squared difference between estimations and ground_truth vectors
+    for(int i=0; i<estimations.size(); i++){
+        VectorXd result = estimations[i] - ground_truth[i];
+        result = result.array() * result.array();
+        rmse += result;
+    }
+
+    // calculate mean
+    rmse = rmse / rmse.size();
+    // calculate rmse
+    rmse = rmse.array().sqrt();
+    return rmse;
 }
